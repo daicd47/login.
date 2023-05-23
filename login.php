1ba47c5a26012ffe872aa2c15f4ec834dc870d64
daicd47 <117286976+daicd47@users.noreply.github.com>
2023-05-23 17:15:34 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,11 @@
 <?php
 // Thiết lập thông tin kết nối đến database
 
-$servername = "database-server-lab7.cocgl5wbv5ga.ap-southeast-
-1.rds.amazonaws.com";
+$servername = "database-server-dai-th7.cizjtuhqargk.ap-southeast-1.rds.amazonaws.com";
 
 $username = "admin";
 $password = "12345678";
-$dbname = "myDB";
+$dbname = "MyDB";
 // Tạo kết nối đến database
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Kiểm tra kết nối
